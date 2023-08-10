ee2fc6115c625fa9267d3dc37a49daab793682bd
Bhadresh-iOS <iosbhadresh@gmail.com>
2023-08-10 09:22:35 +0700

    - init code commit

---
+++ eCom/eCom/eComApp.swift
@@ -0,0 +1,17 @@
+//
+//  eComApp.swift
+//  eCom
+//
+//  Created by Bhadresh on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct eComApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
